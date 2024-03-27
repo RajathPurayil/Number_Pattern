399e4559394d8418607fe53d8fdf36d1593c5f1c
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-27 00:36:27 -0400

    updated with problem statement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Write a program to print below pattern
+
+        /*
+                        1
+                        123
+                        12345
+                        1234567
+
+        */
+
+
+        int  num=1;
+
+        for (int i=1;i<=4;i++){
+
+            for (int j=1;j<=num;j++){
+
+                System.out.print(j);
+            }
+
+            System.out.println();
+
+            num=num+2;
+        }
+
     }
 }
